f567852922ff55d7bd90315d6801663964a0f100
Matt Bierner <mattbierner@gmail.com>
2012-07-29 18:30:09 -0700

    Removed test for other number format alignment, only support decimal numbers.

--- test/testAlign.js
+++ test/testAlign.js
@@ -28,11 +28,6 @@ define(['../lib/ssf'], function(ssf){
             function(){
                 assert.equal(ssf.format("a@(a,c)b", {'a': '1'}), "a1b");
             }],
-            ["Other number formats alignment",
-            function(){
-                assert.equal(ssf.format("a@(a,4.0)b", {'a': '1'}), "a   1b");
-                assert.equal(ssf.format("a@(a,0x4)b", {'a': '1'}), "a   1b");
-            }],
         ],
     };
 });
