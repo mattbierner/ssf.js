306dcff2751ecf9a3419fb552dbd38df82d98e88
Matt Bierner <mattbierner@gmail.com>
2012-08-13 13:32:05 -0700

    Updated string tests for static types.

--- test/testFormatString.js
+++ test/testFormatString.js
@@ -4,38 +4,52 @@ define(['../lib/ssf'], function(ssf){
         'tests': [
             ["String Value",
             function(){
-                assert.equal(ssf.format("abc@", "defg"), 'abcdefg');
+                assert.equal(ssf.format("abc@s()", "defg"), 'abcdefg');
             }],
             ["String Member", 
             function(){
-                assert.equal(ssf.format("a @a", {
+                assert.equal(ssf.format("a @s(a)", {
                     'a': "bla",
                 }), "a bla");
             }],
             ["Empty String", 
             function(){
-                assert.equal(ssf.format("a @a b", ""), "a  b");
+                assert.equal(ssf.format("a @s() b", ""), "a  b");
             }],
             
             ["Format start slice", 
             function(){
-                assert.equal(ssf.format("@(:[1])", "abc"), "bc");
-                assert.equal(ssf.format("@(:[-1])", "abc"), "c");
-                assert.equal(ssf.format("@(:[4])", "abc"), "");
+                assert.equal(ssf.format("@s(:[1])", "abc"), "bc");
+                assert.equal(ssf.format("@s(:[-1])", "abc"), "c");
+                assert.equal(ssf.format("@s(:[4])", "abc"), "");
             }],
             ["Format end slice", 
             function(){
-                assert.equal(ssf.format("@(:[,1])", "abc"), "a");
-                assert.equal(ssf.format("@(:[,-1])", "abc"), "ab");
-                assert.equal(ssf.format("@(:[,4])", "abc"), "abc");
+                assert.equal(ssf.format("@s(:[,1])", "abc"), "a");
+                assert.equal(ssf.format("@s(:[,-1])", "abc"), "ab");
+                assert.equal(ssf.format("@s(:[,4])", "abc"), "abc");
             }],
             ["Format both slice", 
             function(){
-                assert.equal(ssf.format("@(:[1,2])", "abc"), "b");
-                assert.equal(ssf.format("@(:[-2,-1])", "abc"), "b");
-                assert.equal(ssf.format("@(:[1,4])", "abc"), "bc");
+                assert.equal(ssf.format("@s(:[1,2])", "abc"), "b");
+                assert.equal(ssf.format("@s(:[-2,-1])", "abc"), "b");
+                assert.equal(ssf.format("@s(:[1,4])", "abc"), "bc");
             }],
             
+        // Static tests
+            ["Static type test",
+            function(){
+                var t = ssf.compile("@s()");
+                assert.equal(t('abc'), 'abc');
+                assert.equal(t({}), ({}).toString());
+            }],
+            
+            ["Static options test",
+            function(){
+                var t = ssf.compile("@s(:[1,6])");
+                assert.equal(t('abc'), 'bc');
+                assert.equal(t({}), ({}).toString().slice(1, 6));
+            }],
         ],
     };
 });
