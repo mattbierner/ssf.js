a6147d1d2a1cddb5d93d1653e5a7ffd6a456053a
Matt Bierner <mattbierner@gmail.com>
2012-08-13 13:30:06 -0700

    Updated object for static tests.

--- test/testFormatObject.js
+++ test/testFormatObject.js
@@ -4,11 +4,11 @@ define(['../lib/ssf'], function(ssf){
         'tests': [
             ["Object Value",
             function(){
-                assert.equal(ssf.format("@", {}), ({}).toString());
+                assert.equal(ssf.format("@o()", {}), ({}).toString());
             }],
             ["Object Member", 
             function(){
-                assert.equal(ssf.format("@a", {
+                assert.equal(ssf.format("@o(a)", {
                     'a': {},
                 }), ({}).toString());
             }],
@@ -16,9 +16,20 @@ define(['../lib/ssf'], function(ssf){
             function(){
                 var obj = {};
                 obj.toString = function() { return 'bla'; };
-                assert.equal(ssf.format("@", obj), 'bla');
+                assert.equal(ssf.format("@o()", obj), 'bla');
             }],
-      
+            
+        // Static Tests
+            ["Static format test",
+            function(){
+                var t = ssf.compile("@o()");
+                assert.equal(t({}), ({}).toString());
+                assert.equal(t([1,2,3]), ([1,2,3]).toString());
+                
+                var s = [];
+                s.toString = function() { return 'bla'; };
+                assert.equal(t(s), 'bla');
+            }],
         ],
     };
 });
